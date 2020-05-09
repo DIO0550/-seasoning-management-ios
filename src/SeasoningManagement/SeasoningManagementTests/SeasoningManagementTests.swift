b4088103b56ae6c12e15727c820b5f3f08be3de8
DIO0550 <diot12345@yahoo.co.jp>
2020-05-09 22:53:17 +0900

    土台作成。

---
+++ src/SeasoningManagement/SeasoningManagementTests/SeasoningManagementTests.swift
@@ -0,0 +1,34 @@
+//
+//  SeasoningManagementTests.swift
+//  SeasoningManagementTests
+//
+//  Created by DIO on 2020/05/09.
+//  Copyright © 2020 DIO0550. All rights reserved.
+//
+
+import XCTest
+@testable import SeasoningManagement
+
+class SeasoningManagementTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
