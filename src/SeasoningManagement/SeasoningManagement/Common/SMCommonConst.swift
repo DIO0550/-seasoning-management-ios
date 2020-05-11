a9fcb04f59411db1a19d17e72059981773e35af3
DIO0550 <diot12345@yahoo.co.jp>
2020-05-11 22:40:11 +0900

    const追加。

---
+++ src/SeasoningManagement/SeasoningManagement/Common/SMCommonConst.swift
@@ -0,0 +1,13 @@
+//
+//  SMCommonConst.swift
+//  SeasoningManagement
+//
+//  Created by DIO on 2020/05/11.
+//  Copyright © 2020 DIO0550. All rights reserved.
+//
+
+import UIKit
+
+struct SMCommonConst {
+    static let SMUsingSeasoningTableViewCellIndentifier = "SMUsingSeasoningTableViewCell"
+}
