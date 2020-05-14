9b51a65a649814c289a299702c52e11ab5b44cf2
DIO0550 <diot12345@yahoo.co.jp>
2020-05-14 22:37:29 +0900

    定数追加。追加した定数を使用するように変更。

--- src/SeasoningManagement/SeasoningManagement/UsingSeasoningView/SMUsingSeasoningViewController.swift
+++ src/SeasoningManagement/SeasoningManagement/UsingSeasoningView/SMUsingSeasoningViewController.swift
@@ -10,17 +10,26 @@ import UIKit
 
 class SMUsingSeasoningViewController: UIViewController {
     var usingSeasoningList: Array = ["Seasoning1", "Seasoning2", "Seasoning3", "Seasoning1", "Seasoning2", "Seasoning3", "Seasoning1", "Seasoning2", "Seasoning3"]
+    
+    struct SMUsingSeasoningListCollectionViewMergin {
+        static let Left: CGFloat = 10.0
+        static let Right: CGFloat = 10.0
+    }
+    
+    static let SMUsingSeasoningListCollectionViewItemHeight: CGFloat = 150.0
 
     @IBOutlet weak var usingSeasoningListCollectionView: UICollectionView!
     
     override func viewDidLoad() {
-        self.usingSeasoningListCollectionView.register(UINib(nibName: "SMUsingSeasoningCollectionViewCell", bundle: nil), forCellWithReuseIdentifier:"SMUsingSeasoningCollectionViewCell")
+        self.usingSeasoningListCollectionView.register(UINib(nibName: SMCommonConst.SMUsingSeasoningCollectionViewCellIndentifier, bundle: nil), forCellWithReuseIdentifier: SMCommonConst.SMUsingSeasoningCollectionViewCellIndentifier)
         self.setupFlowLayout()
     }
     
     private func setupFlowLayout() {
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: self.usingSeasoningListCollectionView.frame.width - 20, height: 150)
+        let sideMargin = SMUsingSeasoningListCollectionViewMergin.Left + SMUsingSeasoningListCollectionViewMergin.Right
+        layout.itemSize = CGSize(width: self.usingSeasoningListCollectionView.frame.width - sideMargin,
+                                 height: SMUsingSeasoningViewController.SMUsingSeasoningListCollectionViewItemHeight)
         layout.sectionInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
         layout.minimumLineSpacing = 20
         self.usingSeasoningListCollectionView.collectionViewLayout = layout
