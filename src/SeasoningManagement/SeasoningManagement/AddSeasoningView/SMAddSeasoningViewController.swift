498da9a6bef7511afca0d359d7fb36ee31af24ef
DIO0550 <diot12345@yahoo.co.jp>
2020-05-23 18:12:03 +0900

    ファイルの移動、画像なしのアイコン、データ追加画面などを追加

---
+++ src/SeasoningManagement/SeasoningManagement/AddSeasoningView/SMAddSeasoningViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SMAddSeasoningViewController.swift
+//  SeasoningManagement
+//
+//  Created by DIO on 2020/05/23.
+//  Copyright © 2020 DIO0550. All rights reserved.
+//
+
+import UIKit
+
+class SMAddSeasoningViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
