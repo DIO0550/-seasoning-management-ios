9f2773dbd45fbc1480bdf2d39a05b1d1a670cd20
DIO0550 <diot12345@yahoo.co.jp>
2020-05-24 19:26:03 +0900

    IBOoutlet追加。追加ボタンで、CoreData生成処理を追加。

--- src/SeasoningManagement/SeasoningManagement/AddSeasoningView/SMInputAddSeasoningDataViewController.swift
+++ src/SeasoningManagement/SeasoningManagement/AddSeasoningView/SMInputAddSeasoningDataViewController.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
+import RxDataSources
 
 class SMInputAddSeasoningDataViewController: UIViewController {
-
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    @IBOutlet weak var nameLabel: UITextField!
+    @IBOutlet weak var typeLabel: UITextField!
+    @IBOutlet weak var priceLabel: UITextField!
+    @IBOutlet weak var calorieLabel: UITextField!
+    
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.doneBarButton.rx.tap.asDriver().drive(onNext: { [weak self] in
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let seasoningData = appDelegate.createSeasoningData()
+        })
+        .disposed(by: disposeBag)
     }
     
 
