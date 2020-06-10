f7d646955d1132ee305fe75fb252155ec3a11183
DIO0550 <diot12345@yahoo.co.jp>
2020-06-10 21:04:10 +0900

    フレームワーク用のプロジェクト追加

---
+++ src/SeasoningManagerCommon/SeasoningManagerCommon/SeasoningManagerCommon.h
@@ -0,0 +1,19 @@
+//
+//  SeasoningManagerCommon.h
+//  SeasoningManagerCommon
+//
+//  Created by DIO on 2020/06/10.
+//  Copyright © 2020 DIO0550. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SeasoningManagerCommon.
+FOUNDATION_EXPORT double SeasoningManagerCommonVersionNumber;
+
+//! Project version string for SeasoningManagerCommon.
+FOUNDATION_EXPORT const unsigned char SeasoningManagerCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SeasoningManagerCommon/PublicHeader.h>
+
+
