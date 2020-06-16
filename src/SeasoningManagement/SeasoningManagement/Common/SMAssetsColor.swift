8e9056057b3a5a77f817769ed279b41b20b55f90
DIO0550 <diot12345@yahoo.co.jp>
2020-06-16 22:31:28 +0900

    アセット整理

---
+++ src/SeasoningManagement/SeasoningManagement/Common/SMAssetsColor.swift
@@ -0,0 +1,19 @@
+//
+//  SMAssetsColor.swift
+//  SeasoningManagement
+//
+//  Created by DIO on 2020/06/16.
+//  Copyright © 2020 DIO0550. All rights reserved.
+//
+
+import UIKit
+
+struct SMAssetsColor {
+    static let seasoningDataEditTableSectionHeaderColor: UIColor? = {
+        return UIColor.init(named: "SMSeasoningDataEditTableSectionHeaderColor")
+    }()
+    
+    static let seasoningManagementColor: UIColor? = {
+        return UIColor.init(named: "SMSeasoningManagementColor")
+    }()
+}
