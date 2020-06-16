fb7da59e6c40dc4f0784394a5e9d9129cb14c0f5
DIO0550 <diot12345@yahoo.co.jp>
2020-06-16 19:55:57 +0900

    画像の反転処理を実装。

--- src/SeasoningManagement/SeasoningManagement/Extension/UIImageEx.swift
+++ src/SeasoningManagement/SeasoningManagement/Extension/UIImageEx.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 extension UIImage {
+    
+    /**
+     上下反転している画像を修正する
+     
+     @return 修正した画像
+     */
     func fixedFlipImage() -> UIImage? {
         if self.imageOrientation == .up {
             return self;
@@ -20,12 +26,13 @@ extension UIImage {
         }
         
         context.translateBy(x: self.size.width / 2.0, y: self.size.height / 2)
+        context.scaleBy(x: 1.0, y: -1.0)
         context.rotate(by: CGFloat.pi)
         self.draw(in: CGRect(x: -self.size.width/2, y: -self.size.height / 2, width: self.size.width, height: self.size.height))
 
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        let fixedFlipImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
 
-        return newImage
+        return fixedFlipImage
     }
 }
